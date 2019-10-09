e67f6f0ef0717a90e2f23ebe7d39a070aeca01a6
Benjamin Geer <benjaminlewis.geer@unibas.ch>
2019-10-09 16:17:49 +0200

    Implement data import.

--- import.py
+++ import.py
@@ -3,6 +3,13 @@
 import argparse
 import nltk
 import re
+from os import listdir
+from os.path import isfile, join, splitext
+import tempfile
+from typing import List, Set, Dict, Tuple, Optional
+import json
+from jsonschema import validate
+from knora import KnoraError, KnoraStandoffXml, Knora, Sipi
 
 
 pos_to_xml = {
@@ -52,9 +59,6 @@ def add_markup(input_file_path, output_file_path):
 
         input_file_content = input_file.read()
 
-    print(f"Title: {title}")
-    print(f"Author: {author}")
-
     tokens = nltk.word_tokenize(input_file_content)
     tagged = nltk.pos_tag(tokens)
 
@@ -100,13 +104,43 @@ def add_markup(input_file_path, output_file_path):
 
         print("</text>", file=output_file)
 
+    return author, title
+
+
+def do_import(input_dir_path):
+    temp_dir_path = tempfile.mkdtemp()
+    print(f"Using temporary directory {temp_dir_path}")
+    input_filenames = [file_path for file_path in listdir(input_dir_path) if isfile(join(input_dir_path, file_path))]
+    con = Knora("http://0.0.0.0:3333")
+    con.login("testuser@test.org", "test")
+    schema = con.create_schema("00FD", "books")
+
+    for input_filename in input_filenames:
+        print(f"Processing {input_filename}...")
+        input_filename_without_ext, _ = splitext(input_filename)
+        input_file_path = join(input_dir_path, input_filename)
+        output_file_path = join(temp_dir_path, f"{input_filename_without_ext}.xml")
+        author, title = add_markup(input_file_path, output_file_path)
+
+        with open(output_file_path, "r", encoding="utf-8") as xml_file:
+            xml_content = xml_file.read()
+
+            resource_info = con.create_resource(schema, "book", f"{input_filename_without_ext}", {
+                "hasAuthor": f"{author}",
+                "hasTitle": f"{title}",
+                "hasText": {
+                    "value": KnoraStandoffXml(xml_content)
+                }
+            })
+
+            print(f"Created resource {resource_info['iri']}")
+
 
 def main():
     parser = argparse.ArgumentParser(description="Imports Project Gutenberg books into Knora, adding markup.")
-    parser.add_argument("input", help="input file")
-    parser.add_argument("output", help="output file")
+    parser.add_argument("input", help="input directory")
     args = parser.parse_args()
-    add_markup(input_file_path=args.input, output_file_path=args.output)
+    do_import(input_dir_path=args.input)
 
 
 if __name__ == "__main__":
